43e42441ec478a7dff72df51341aa2dce90bfb80
Serena Gibbons <serenalgibbons@gmail.com>
2019-09-18 15:55:15 -0400

    Add Snackbar message

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething(View view) {
+        Snackbar.make(view,"Snackbar msg",Snackbar.LENGTH_SHORT).show();
     }
 }
